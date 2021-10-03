eebea40f34be03c4fbd06e8e99e1025ec108457e
Kieran <45768489+wadforth@users.noreply.github.com>
2021-10-03 17:32:19 +0100

    Added show compatibility

--- Trakt link to Ask4Movie.user.js
+++ Trakt link to Ask4Movie.user.js
@@ -6,7 +6,7 @@
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @license     Unlicense; http://unlicense.org/
-// @version     0.1
+// @version     0.2
 // @match       *://trakt.tv/*
 // @run-at document-start
 // @inject-into     page
@@ -16,17 +16,29 @@
 $(function() {
     'use strict'
 
+    var url2String = window.location.href.toString();
     const element = document.getElementsByClassName('mobile-title')[0];
+    const certification = document.getElementsByClassName('certification')[0];
+    certification.remove();
     var button = document.createElement('button');
     button.type = 'button';
     button.innerHTML = '<img src="https://ask4movie.cc/wp-content/uploads/2018/04/the-logo.png" style="display: block; margin-left: auto; margin-right: auto; width: 50%;"/>';
     button.className = 'btn-styled';
+    console.log(element.innerText);
 
+    //Searches show direct, should show all seasons
+    if (url2String.contains('shows')) {
+        button.onclick = function() {
+        window.location.href = 'https://ask4movie.io/?s=' + element.innerText;
+        console.log(url2String + element.innerText);
+    };
+   }
+    //Links directly to the movie if its available on website
+    else if (url2String.contains('movies')) {
     button.onclick = function() {
-    var getUrl = window.location.href.toString();
-    var baseUrl = window.location = getUrl.replace('trakt.tv/movies/', 'ask4movie.io/');
+    var baseUrl = window.location = url2String.replace('trakt.tv/movies/', 'ask4movie.io/');
     console.log(baseUrl);
     };
-
-    element.appendChild(button);
+    }
+element.appendChild(button);
 })();
