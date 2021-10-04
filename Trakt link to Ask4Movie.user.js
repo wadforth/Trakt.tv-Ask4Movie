de321f687075466274d716a899d48a1e3850338c
Kieran <45768489+wadforth@users.noreply.github.com>
2021-10-04 23:31:02 +0100

    uncaught typeerror #2 fix
    When no streaming options available, button will show just underneath.

--- Trakt link to Ask4Movie.user.js
+++ Trakt link to Ask4Movie.user.js
@@ -6,7 +6,7 @@
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @license     Unlicense; http://unlicense.org/
-// @version     1.1
+// @version     1.2
 // @updateURL   https://github.com/wadforth/Trakt.tv-Ask4Movie/raw/main/Trakt%20link%20to%20Ask4Movie.user.js
 // @match       *://trakt.tv/*
 // @match       *://trakt.tv/shows/*/seasons/*
@@ -21,13 +21,13 @@ $(function() {
     const element = document.getElementsByClassName('mobile-title')[0];
     const certification = document.getElementsByClassName('certification')[0];
     const streaming = document.getElementsByClassName('streaming-links')[0];
+    const streamingalt = document.getElementsByClassName('btn-watch-now')[0];
     const date = document.getElementsByClassName('year')[0];
     if ($(".certification")[0]){
     certification.remove();
-}
+    }
 
     var button = document.createElement('div');
-    button.innerHTML = '<a href=""><img src="https://ask4movie.io/wp-content/uploads/2021/09/cropped-Icon-Logo-32x32.png" style="display: block; margin-left: auto; margin-right: auto; width: 50%;"/></a>';
 
     //Searches show direct, should show all seasons
     if (url2String.contains('shows') && !url2String.contains('seasons')) {
@@ -53,5 +53,11 @@ $(function() {
             window.location.href = 'https://ask4movie.io/' + show + "-" + seasons;
         }
     }
+    if ($(".streaming-links")[0]){
+    button.innerHTML = '<a href=""><img src="https://ask4movie.io/wp-content/uploads/2021/09/cropped-Icon-Logo-32x32.png" style="display: block; margin-left: auto; margin-right: auto; width: 50%;"/></a>';
     streaming.appendChild(button);
+} else {
+    button.innerHTML = '<a href=""><img src="https://ask4movie.io/wp-content/uploads/2021/09/cropped-Icon-Logo-32x32.png" style="display: block; margin-left: auto; margin-right: auto; width: 25%;"/></a>';
+    streamingalt.appendChild(button);
+}
 });
