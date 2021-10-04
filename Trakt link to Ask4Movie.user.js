f2f947edbcf8f726c3f9381dd9ce6382e1a68c06
Kieran <45768489+wadforth@users.noreply.github.com>
2021-10-04 01:36:04 +0100

    Added compatibility for show seasons

--- Trakt link to Ask4Movie.user.js
+++ Trakt link to Ask4Movie.user.js
@@ -6,38 +6,49 @@
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @license     Unlicense; http://unlicense.org/
-// @version     0.3
+// @version     1.0
 // @updateURL   https://github.com/wadforth/Trakt.tv-Ask4Movie/raw/main/Trakt%20link%20to%20Ask4Movie.user.js
 // @match       *://trakt.tv/*
+// @match       *://trakt.tv/shows/*/seasons/*
 // @run-at document-start
 // @inject-into     page
 /* global $ */
 // ==/UserScript==
-
 $(function() {
     'use strict'
 
     var url2String = window.location.href.toString();
     const element = document.getElementsByClassName('mobile-title')[0];
     const certification = document.getElementsByClassName('certification')[0];
+    const streaming = document.getElementsByClassName('streaming-links')[0];
+    const date = document.getElementsByClassName('year')[0];
     certification.remove();
     var button = document.createElement('div');
-    button.innerHTML = '<a href=""><img src="https://ask4movie.io/wp-content/uploads/2021/09/cropped-Icon-Logo-32x32.png" style="display: block; width: 3%;"/></a>';
-    console.log(element.innerText);
+    button.innerHTML = '<a href=""><img src="https://ask4movie.io/wp-content/uploads/2021/09/cropped-Icon-Logo-32x32.png" style="display: block; margin-left: auto; margin-right: auto; width: 50%;"/></a>';
 
     //Searches show direct, should show all seasons
-    if (url2String.contains('shows')) {
+    if (url2String.contains('shows') && !url2String.contains('seasons')) {
         button.onclick = function() {
-        window.location.href = 'https://ask4movie.io/?s=' + element.innerText;
-        console.log(url2String + element.innerText);
-    };
-   }
+            window.location.href = 'https://ask4movie.io/?s=' + element.innerText;
+            console.log(url2String + element.innerText);
+        };
+    }
     //Links directly to the movie if its available on website
     else if (url2String.contains('movies')) {
-    button.onclick = function() {
-    var baseUrl = window.location = url2String.replace('trakt.tv/movies/', 'ask4movie.io/');
-    console.log(baseUrl);
-    };
+        button.onclick = function() {
+            var baseUrl = window.location = url2String.replace('trakt.tv/movies/', 'ask4movie.io/');
+            console.log(baseUrl);
+        };
     }
-element.appendChild(button);
+
+    if (url2String.contains('seasons') && !url2String.contains('episodes')) {
+        date.remove();
+        const show = element.childNodes[0].textContent.split(" ").join("-");
+        const seasons = element.childNodes[1].textContent.split(" ").join("-");;
+        button.onclick = function() {
+
+            window.location.href = 'https://ask4movie.io/' + show + "-" + seasons;
+        }
+    }
+    streaming.appendChild(button);
 })();
